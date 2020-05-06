4af91e0f4d67f14a376ff15ab33752e3d547f5ba
Quinn Elder <qre.turing@gmail.com>
2020-05-06 17:15:46 -0600

    Add CSS changes to coresponding Components already with CSS styling

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -14,7 +14,7 @@ const NewsArticle = ({
       <h2 className="article-header">{headline}</h2>
       <img src={img} className="article-img" alt="News article"></img>
       <p className="article-description" >{description}</p>
-      <a href={url} className="article-link" >VIEW THE LINK =></a>
+      <p className='links'><a href={url} className="article-link">VIEW THE LINK</a></p>
     </section>
   );
 }

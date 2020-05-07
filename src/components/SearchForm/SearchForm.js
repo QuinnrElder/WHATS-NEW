506871047b77557ed20fe369878c2ff9a64237d7
Quinn Elder <qre.turing@gmail.com>
2020-05-07 16:27:55 -0600

    Add CSS to SearchForm.js as well as some refactoring

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './SearchForm.css';
 
-const SearchForm = () => {
+const SearchForm = (props) => {
+  console.log(props)
   return (
     <section className="header-container">
       <h2 className="header">WHAT'S NEW?!?</h2>
-      <input></input>
-      <button>search</button>
+      <input className="user-input"
+      name="searchInput"></input>
+      <button className="search-btn" onClick={props.onClick}>search</button>
     </section>
   )
 }
